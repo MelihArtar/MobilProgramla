07a463bfca96686fc54e205a8d2d01f88e077dc6
Melih Artar <92114880+MelihArtar@users.noreply.github.com>
2023-11-29 19:54:48 +0300

    Ödev 7
    Ödev 7

--- main.dart
+++ main.dart
@@ -1,116 +1,129 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('55'),
-        ],
-      ),
-    );
-
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, 'CALL'),
-        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-        _buildButtonColumn(color, Icons.share, 'SHARE'),
-      ],
-    );
-
-    Widget textSection = const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-
-    return MaterialApp(
-      title: 'Flutter layout',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Login Pace',
+          style: TextStyle(color: Colors.white),
         ),
-        body: ListView(
-          children:[
-            Image.asset(
-              'images/lake.jpg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover,
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 64.0),
+            Container(
+              margin: const EdgeInsets.all(15.0),
+              height: 80.0,
+              child: const FlutterLogo(
+                style: FlutterLogoStyle.horizontal,
+                size: 512,
+              ),
             ),
-            titleSection,
-            buttonSection,
-            textSection,
+            SizedBox(height: 32.0),
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            Container(
+              child: TextButton(
+                onPressed: () {
+                  // Düğmeye tıklandığında yapılacak işlemler buraya yazılır.
+                  print('TextButton\'a tıklandı!');
+                },
+                child: const Text(
+                  'Forgot Password',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Burada kullanıcı adı ve şifreyi kontrol edebilirsiniz.
+                String username = _usernameController.text;
+                String password = _passwordController.text;
+
+                // Örnek olarak, basit bir kontrol yapalım.
+                if (username == 'user' && password == 'password') {
+                  // Kullanıcı girişi başarılı
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Login successful!'),
+                    ),
+                  );
+                } else {
+                  // Kullanıcı girişi başarısız
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content:
+                          Text('Login failed. Please check your credentials.'),
+                    ),
+                  );
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                fixedSize: Size(250, 20),
+              ),
+              child: const Text(
+                'Login',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                ),
+              ),
+            ),
+            const SizedBox(height: 64.0),
+            Container(
+              margin: const EdgeInsets.all(0.0),
+              child: TextButton(
+                onPressed: () {
+                  // Düğmeye tıklandığında yapılacak işlemler buraya yazılır.
+                  print('TextButton\'a tıklandı!');
+                },
+                child: const Text(
+                  'New User? Create Account',
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+            ),
           ],
         ),
       ),
-    );
-  }
-
-
-
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
